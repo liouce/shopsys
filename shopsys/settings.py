0f7f0841988c60732eeb78156e932c99bbec1c45
lwl <lwl_liouce@163.com>
2017-09-03 21:50:58 +0800

    修改数据库引擎为mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'json1',
+        'USER': 'testuser1',
+        'PASSWORD': '123456',
+        'HOST': '192.168.1.5',
+        'PORT': '3306',
     }
 }
 
